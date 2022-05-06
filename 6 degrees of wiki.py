226f118c31b213e898ce3e28fbf9f459a7092e2e
Michael Durham <mjdurham99@gmail.com>
2022-05-06 12:52:34 -0400

    Added a method for parsing out all of wikipedia links in article.

--- 6 degrees of wiki.py
+++ 6 degrees of wiki.py
@@ -1,10 +1,18 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import random
+import datetime
 import re
-html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
-bs = BeautifulSoup(html, 'html.parser')
-for link in bs.find('div', {'id':'bodyContent'}).find_all(
-'a', href=re.compile('^(/wiki/)((?!:).)*$')):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('div', {'id':'bodyContent'}).find_all('a',href=re.compile('^(/wiki/)((?!:).)*$'))
+
+    
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+    print(newArticle)
+    links = getLinks(newArticle)
