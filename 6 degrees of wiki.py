77c712359724c3197ad15ed4714ee9e021235240
Michael Durham <mjdurham99@gmail.com>
2022-05-06 13:21:55 -0400

    Completed functionality of web scraper and added finishing touches"

--- 6 degrees of wiki.py
+++ 6 degrees of wiki.py
@@ -5,14 +5,24 @@ import random
 import datetime
 import re
 random.seed(datetime.datetime.now())
+degrees = int(input("How many degrees of bacon?:"))
+current_articules = 0
 def getLinks(articleUrl):
     html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
     bs = BeautifulSoup(html, 'html.parser')
     return bs.find('div', {'id':'bodyContent'}).find_all('a',href=re.compile('^(/wiki/)((?!:).)*$'))
 
-    
+def readTitle(articleUrl):
+    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    for title in bs.find('h1', 'firstHeading'): 
+        return title
+
 links = getLinks('/wiki/Kevin_Bacon')
-while len(links) > 0:
+while len(links) > 0 and current_articules < degrees:
     newArticle = links[random.randint(0, len(links)-1)].attrs['href']
-    print(newArticle)
-    links = getLinks(newArticle)
+    current_articules += 1
+    print(current_articules, ": ", newArticle)
+    if(current_articules<degrees):
+        links = getLinks(newArticle)
+print(readTitle(newArticle))
